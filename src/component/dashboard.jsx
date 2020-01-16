85bcd107b8078dbd6c6bcd117e0d38238ffdc858
pooja todkar <poojatodkar124@gmail.com>
2020-01-16 15:25:46 +0530

    Refactored: Dashboard component changed

--- src/component/dashboard.jsx
+++ src/component/dashboard.jsx
@@ -1,32 +1,28 @@
 import React, { Component } from 'react';
-import'../css/dashboard.css'
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import '../css/dashboard.css'
+import '../assets/book.png';
+import TextField from '@material-ui/core/TextField';
+
 
 class Dashboard extends Component {
 
     render() {
         console.log("this.props", this.props)
 
-        return <div>
-            {/* <AppBar position="fixed">
-                <Toolbar>
-                    <IconButton edge="start" color="inherit" aria-label="menu">
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6">
-                        Tall Tales Book Store
-                    </Typography>
-                </Toolbar>
-            </AppBar> */}
-            <div className="toolBar">
-                <h1 className="bookTitle">Tall Tales Book Store</h1>
+        return <div className="toolBar">
+
+            <div className="bookTitle">
+                Tall Tales Book Store
+
+                <div className="searchBar"> 
+                    <form>
+                    <TextField id="outlined-basic" variant="outlined" />
+                </form>
                 </div>
+            </div>
+
         </div>
+
     }
 
 }
