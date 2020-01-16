ad16373b28ad81ea006c54cdccd3a0137ccf82c8
pooja todkar <poojatodkar124@gmail.com>
2020-01-16 11:07:15 +0530

    Project Structure Done

---
+++ src/component/dashboard/dashboard.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './dashboard.css';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+class Dashboard extends Component {
+
+    render() {
+        console.log("this.props", this.props)
+
+        return <div>
+            <AppBar position="fixed">
+                <Toolbar>
+                    <IconButton edge="start" color="inherit" aria-label="menu">
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6">
+                        Tall Tales Book Store
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </div>
+    }
+
+}
+
+export default Dashboard;
